b344c265db536f471510bcad3ee29b41a9187875
Navneet <navneet26022008@gmail.com>
2025-10-29 21:16:25 +0530

    Implement function to add two numbers represented as lists

---
+++ Add Two Numbers.py
@@ -0,0 +1,18 @@
+def addTwoNumbers(l1, l2):
+    head = cur = ListNode(0)
+    carry = 0
+
+    while l1 or l2 or carry:
+        v1 = l1.val if l1 else 0
+        v2 = l2.val if l2 else 0
+        
+        s = v1 + v2 + carry
+        carry = s // 10
+        
+        cur.next = ListNode(s % 10)
+        cur = cur.next
+
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+
+    return head.next
